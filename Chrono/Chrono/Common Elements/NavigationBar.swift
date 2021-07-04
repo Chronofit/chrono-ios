ff6ec32f39c12b8c013c08e0dfc94e4d63cb2ea7
Hari Govind <grhari1998@gmail.com>
2021-07-04 17:59:23 -0400

    Transfer string constants from android app

--- Chrono/Chrono/Common Elements/NavigationBar.swift
+++ Chrono/Chrono/Common Elements/NavigationBar.swift
@@ -57,3 +57,9 @@ extension View {
     }
 }
 
+
+struct NavigationBar_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
