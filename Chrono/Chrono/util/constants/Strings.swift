43832ffe6a153949494b4195f875a5b9c7aed1a8
Hari Govind <grhari1998@gmail.com>
2021-07-07 20:39:12 -0400

    Follow variable id's to follow name linter rules

---
+++ Chrono/Chrono/util/constants/Strings.swift
@@ -0,0 +1,127 @@
+//
+//  Strings.swift
+//  Chrono
+//
+//  Created by Hari Govind on 2021-07-07.
+//
+
+import Foundation
+struct Strings {
+    static let appName = "Chrono"
+
+    // Timer Controls
+    static let stop = "Stop"
+    static let start = "Start"
+    static let pause = "Pause"
+    static let resume = "Resume"
+    static let lap = "Lap"
+    static let chronometer = "Chronometer"
+
+    // Circuit Create
+    static let const0 = "0"
+    static let cont5 = "5"
+    static let cont30 = "30"
+    static let nameInputHint = "Name of Circuit"
+    static let save = "Save"
+    static let discard = "Discard"
+    static let todo = "TODO"
+    static let placeholder = "Placeholder"
+    static let sets = "Sets"
+    static let dismiss = "Dismiss"
+    static let selectAnIcon = "Select an Icon"
+    static let sec = "sec"
+    static let rest = "Rest"
+    static let work = "Work"
+
+    static let workout = "Workout"
+    static let getReady = "Get Ready"
+    static let circuitName = "Circuit Name"
+    static let name = "Name"
+    static let letsGo = "Let's Go!"
+
+    // Tab Navigation
+    static let settings = "Settings"
+    static let circuits = "Circuits"
+    static let stopwatch = "Stopwatch"
+
+    // Delete Circuit Dialog
+    static let deleteCicuit = "Delete Circuit"
+    static let deleteCircuitSubtitle = "Do you want to delete this circuit?"
+    static let delete = "Delete"
+    static let cancel = "Cancel"
+
+    // Circuit Complete Dialog
+    static let circuitComplete = "Circuit Complete🥳🥳🥳"
+    static let circuitCompleteSubtitle = "Woohoo! You have completed your circuit, good job!!!"
+    static let circuitCompleteConfirm = "Finish"
+    static let circuitCompleteCancel = "Restart"
+    static let circuitCompleteCelebrate = "Circuit Complete!"
+
+    // Easter Egg Dialog
+    static let easterEgg = "🐇🥚"
+    static let easterEggSubtitle = "Congrats! You just unlocked the super secret stats page. Soon, this will be available to everyone, but right now it\'s an easter egg 🤫"
+    static let easterEggConfirm = "Continue"
+    static let stats = "Stats"
+    static let totalTime = "Total Time"
+    static let totalCircuits = "Total Circuits"
+    static let staticTimer = "0h 0m"
+    static let staticStopwatch = "0h 0m 0s"
+
+    // Settings
+    static let notifications = "Notifications"
+    static let on = "On"
+    static let darkMode = "Dark Mode"
+    static let off = "Off"
+    static let circuitTimer = "Circuit Timer"
+    static let preferences = "Preferences"
+    static let audioPrompts = "Audio Pompts"
+    static let about = "About"
+    static let set = "Set"
+    static let privacyPolicy = "Privacy Policy"
+    static let getReadyTime = "Get Ready Time"
+    static let timeEmoji = "⏳"
+    static let darkEmoji = "🕶️"
+    static let const5s = "5s"
+    static let const10s = "10s"
+    static let const15s = "15s"
+    static let rateApp = "Rate App"
+    static let getHelp = "Get Help"
+    static let skipLastRest = "Skip Last Rest"
+    static let selectIconSubtitle = "Use this icon to represent your circuit"
+
+    static let chronoLogo = "Chrono Logo"
+    static let changeNicon = "Change\nIcon"
+    static let maxCharWarning = "Maximum 50 characters."
+    static let comingSoon = "Coming Soon"
+    static let systemDefault = "System Default"
+    static let theNumberOfSecondsBeforeYourCircuitBegins = "The number of seconds before your circuit begins"
+    static let enterTheDarkSide = "Enter the dark side"
+    static let edit = "Edit"
+    static let share = "Share"
+    static let updateAvailableTitle = "Update Available 📲"
+    static let updateAvailableSubtitle = "You are on an older version of Chrono. Please update your app for the latest features and bug fixes."
+    static let update = "Update"
+    static let helpEmailSubject = "Chrono Support Ticket"
+    static let helpEmailBody = "Please let us know what you need help with. Feel free to include screenshots:"
+    static let soundEffect = "Sound Effect"
+    static let speakerEmoji = "🔊"
+    static let soundEffectSubtitle = "Pick the sound you like for your circuits"
+    static let shortWhistle = "Short Whistle"
+    static let longWhistle = "Long Whistle"
+    static let next = "Next"
+    static let skip = "Skip"
+    static let continueButton = "Continue"
+
+    // Onboarding Titles/Subtitles
+    static let onboardWelcomeTitle = "Welcome"
+    static let onboardWelcomeSubtitle = "Thank you for choosing Chrono! Let\'s\nwalk you through how our app\nworks."
+    static let onboardCircuitTitle = "Save custom circuits"
+    static let onboardCircuitSubtitle = "Customize your circuit with your\n desired times, title, and icon. Click on\n your saved circuit to run it."
+    static let onboardSwatchTitle = "Use the stopwatch"
+    static let onboardSwatchSubtitle = "With integrated lap functionality, our\n stopwatch will help you time\n basically anything."
+    static let onboardSettingTitle = "Dark mode"
+    static let onboardSettingSubtitle = "Use the settings page to personalize\nyour Chrono experience. Also there\'s\n dark mode. 🖐 🎤 ⬇"
+    static let onboardFinalTitle = "You\'re all set"
+    static let onboardFinalSubtitle = "Feel free to leave us feedback and\" joining our Discord server. We\n hope you enjoy using Chrono!"
+    static let discord = "Discord"
+}
---
+++ Chrono/Chrono/util/constants/links.swift
@@ -0,0 +1,14 @@
+//
+//  links.swift
+//  Chrono
+//
+//  Created by Hari Govind on 2021-07-07.
+//
+
+import Foundation
+struct links {
+    static let privacyPageLink = "https://chronofit.herokuapp.com/privacyPolicy"
+    static let discordInviteLink = "https://discord.gg/UbWrRxwkgR"
+    static let appRatingPrimaryLink = "TODO"
+    static let appRatingSecondaryLink = "TODO"
+}
