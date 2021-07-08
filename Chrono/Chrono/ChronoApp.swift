12ff51bbc6c6fff121debaa20b5d73ffd13445e7
Sagar Patel <s.72427patel@gmail.com>
2021-07-07 20:51:33 -0400

    Working Integration for Firebase Analytics/Crashlytics (#1)

--- Chrono/Chrono/ChronoApp.swift
+++ Chrono/Chrono/ChronoApp.swift
@@ -12,6 +12,12 @@ import SwiftUI
 struct ChronoApp: App {
     init() {
         FirebaseApp.configure()
+        
+        Analytics.logEvent(AnalyticsEventSelectContent, parameters: [
+          AnalyticsParameterItemID: "id-App Started)",
+          AnalyticsParameterItemName: "App Started",
+          AnalyticsParameterContentType: "cont",
+        ])
     }
 
     var body: some Scene {
